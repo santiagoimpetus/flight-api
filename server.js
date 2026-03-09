6fed667ef3daf0d87a462dbc25efe4eaefdcea6e
Rodrigo Santiago <rodrigosantiago@MacBook-Pro-de-Rodrigo.local>
2026-03-09 16:59:15 -0300

    fix port for railway

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.status(200).json({ ok: true, route: "/" });
